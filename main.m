a30e3aa1eeadda241a08cb076dbbe34f0ab68d2f
kitchell <lindseykitchell21@gmail.com>
2017-09-26 11:38:12 -0400

    update

--- main.m
+++ main.m
@@ -3,10 +3,10 @@ function [] = main()
 switch getenv('ENV')
     case 'IUHPC'
         disp('loading paths for IUHPC')
-        %addpath(genpath('/N/u/hayashis/BigRed2/git/encode'))
-        addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'))
-        addpath(genpath('/N/u/hayashis/BigRed2/git/jsonlab'))
-        %addpath(genpath('/N/u/hayashis/BigRed2/git/afq-master'))
+        %addpath(genpath('/N/u/brlife/git/encode'))
+        addpath(genpath('/N/u/brlife/git/vistasoft'))
+        addpath(genpath('/N/u/brlife/git/jsonlab'))
+        %addpath(genpath('/N/u/brlife/git/afq-master'))
     case 'VM'
         disp('loading paths for Jetstream VM')
         %addpath(genpath('/usr/local/encode'))
@@ -18,9 +18,13 @@ end
 % load my own config.json
 config = loadjson('config.json');
 %config.dt6 config.afq
-
+%dt = dtiLoadDt6('/N/dc2/projects/lifebid/code/kitchell/app-tractanalysisprofiles/dtiinit/dti/dt6.mat');
 dt = dtiLoadDt6(fullfile(config.dt6,'/dti/dt6.mat'));
 load(config.afq);
+%load('output.mat');
+prop = config.diff_prop;
+
+mkdir('images');
 
 for ifg = 1:length(fg_classified)
     fg = fg_classified( ifg );
@@ -37,22 +41,36 @@ for ifg = 1:length(fg_classified)
     nodesToPlot = 50:151;
     
     h.tpfig = figure('name', 'My tract profile','color', 'w', 'visible', 'off');
-    tract_profile = plot(fa(nodesToPlot),'color', [0.2 0.2 0.9],'linewidth',4)
+    
+    switch prop
+        case 'fa'
+            tract_profile = plot(fa(nodesToPlot),'color', [0.2 0.2 0.9],'linewidth',4)
+            ylh = ylabel('Fractional Anisotropy');
+        case 'md'
+            tract_profile = plot(md(nodesToPlot),'color', [0.2 0.2 0.9],'linewidth',4)
+            ylh = ylabel('Mean Diffusivity');
+        case 'rd'
+            tract_profile = plot(rd(nodesToPlot),'color', [0.2 0.2 0.9],'linewidth',4)
+            ylh = ylabel('Radial Diffusivity');
+        case 'ad'
+            tract_profile = plot(ad(nodesToPlot),'color', [0.2 0.2 0.9],'linewidth',4)
+            ylh = ylabel('Axial Diffusivity');
+    end
     set(gca, 'fontsize',20, 'box','off', 'TickDir','out', ...
         'xticklabel',{'Tract begin','Tract end'},'xlim',[0 100],'ylim',[0.00 1.00],'Ytick',[0 .25 .5 .75],'Xtick',[0 100])
     Title_plot = title(fg.name);
     xlabel('Location on tract')
-    ylh = ylabel('Fractional Anisotropy');
     
-    saveas(tract_profile, fullfile('images/', Title_plot.String), 'png')
-    saveas(tract_profile, fullfile('images/', Title_plot.String), 'eps')
+    
+    saveas(tract_profile, fullfile('images/', Title_plot.String, ' ', prop), 'png')
+    saveas(tract_profile, fullfile('images/', Title_plot.String, ' ', prop), 'eps')
     %clear()
-    json.images(ifg).filename = strcat(Title_plot.String,'.png');
-    json.images(ifg).name = strcat(Title_plot.String, ' tract analysis profile');
+    json.images(ifg).filename = strcat('images/',Title_plot.String,' ', prop, '.png');
+    json.images(ifg).name = strcat(Title_plot.String);
     json.images(ifg).desc = strcat(Title_plot.String, ' tract analysis profile');
     
 end
-
+clf
 savejson('', json, fullfile('images.json'));
 end
 
